f825d7d28bd7a0ab59dbb57fca27c254c5d9b8b2
Malik Ahsan <malikahsan13@gmail.com>
2025-04-26 01:04:45 +0500

    created user model, register route and registed user successfully

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,30 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import authRoutes from "./routes/auth"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const URI = process.env.MONGO_URI;
+console.log(URI)
+if(!URI){
+    throw new Error("MongoDB URI is not correct")
+}
 app.use(express.json())
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
 mongoose
-    .connect(process.env.MONGO_URI!)
+    .connect(URI)
     .then(() => {
         console.log("Connected to MongoDB");
         app.listen(PORT, () => {
-            console.log("Server running on http://localhost:${PORT}")
+            console.log(`Server running on http://localhost:${PORT}`)
         })
     })
     .catch((err) => {
