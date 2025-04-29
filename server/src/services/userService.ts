51acef6a3866acc66367dfc89094bc051afbfe1b
Malik Ahsan <malikahsan13@gmail.com>
2025-04-29 23:59:33 +0500

    added user service

---
+++ server/src/services/userService.ts
@@ -0,0 +1,23 @@
+import { AppDataSource } from "../db";
+import { User } from "../entities/user.entity";
+
+export class UserService {
+  private userRepository = AppDataSource.getRepository(User);
+
+  async getAll() {
+    return this.userRepository.find();
+  }
+
+  async getById(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async update(id: number, payload: Partial<User>) {
+    await this.userRepository.update(id, payload);
+    return this.getById(id);
+  }
+
+  async delete(id: number) {
+    return this.userRepository.delete(id);
+  }
+}
