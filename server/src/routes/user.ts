932297625118b2d771bc26afea9b8984bec9e1f5
Malik Ahsan <malikahsan13@gmail.com>
2025-05-01 23:56:15 +0500

    fixed import issues

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { UserService } from "../services/user.service";
+import { UserService } from "../services/userService";
 
 const router = Router();
 const userService = new UserService();
