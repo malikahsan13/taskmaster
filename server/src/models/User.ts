8579e4ed0af810993c4925b406a4dad59b77b238
Malik Ahsan <malikahsan13@gmail.com>
2025-05-04 22:43:18 +0500

    added role key in user model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,12 +4,18 @@ interface IUserDocument extends Document{
     name: string;
     email: string;
     password: string;
+    role: 'user' | 'admin'
 }
 
 const userSchema = new Schema({
     name: {type: String, required: true},
     email: { type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    role: {
+        type: String,
+        enum: ['user','admin'],
+        default: 'user'
+    }
 }, {timestamps: true})
 
 export const User = model<IUserDocument>('User', userSchema)
