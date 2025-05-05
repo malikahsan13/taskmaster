930d55037239eb0d8d3725c3282978985c91313a
Malik Ahsan <malikahsan13@gmail.com>
2025-05-05 23:57:03 +0500

    added welcome email code in user registration

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express"
 import { registerUser, loginUser, getUserById } from "../services/authService"
+import { sendMail } from "../utils/mailer";
 
 export const register = async (req: Request, res: Response) => {
     try{
         const {name, email, password} = req.body
         const user = await registerUser({name, email, password})
+        await sendMail(
+            email,
+            "Welcome to Our App 🎉",
+            `<h1>Hello, ${name}</h1><p>Thanks for registering!</p>`
+          );
         res.status(201).json(user)
     }catch(error : any){
         res.status(500).json({message: error.message || "Server error"})
