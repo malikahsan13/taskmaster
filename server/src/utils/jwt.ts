3a84c8990fc98dcb93b60d040617371f757024bf
Malik Ahsan <malikahsan13@gmail.com>
2025-05-07 23:50:12 +0500

    added refresh token method in jwt utils

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -4,3 +4,7 @@ import { JwtPayload } from "../types/user"
 export const generateToken = (payload: JwtPayload) => {
     return jwt.sign(payload, process.env.JWT_SECRET!, {expiresIn: "1h"})
 }
+
+export const generateRefreshToken = (payload: object) => {
+    return jwt.sign(payload, process.env.JWT_REFRESH_SECRET!, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY });
+  };
