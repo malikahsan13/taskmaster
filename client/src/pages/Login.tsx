b4e36de3137b04f6aab09d723b421d740368ab10
Malik Ahsan <malikahsan13@gmail.com>
2025-05-09 23:46:26 +0500

    added react login page code

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import api from "../api/axios";
+
+const Login = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/login", form);
+      localStorage.setItem("token", res.data.token);
+      alert("Login successful!");
+    } catch (err: any) {
+      alert(err.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="email" value={form.email} onChange={handleChange} placeholder="Email" />
+      <input type="password" name="password" value={form.password} onChange={handleChange} placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
