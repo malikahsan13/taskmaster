cdb806c6dc77de91b3f2daa46ca8c3fdeecd3618
Malik Ahsan <malikahsan13@gmail.com>
2025-05-14 23:02:21 +0500

    create an instance that refreshes the token when needed

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -5,4 +5,19 @@ const api = axios.create({
   withCredentials: true
 });
 
+api.interceptors.response.use(
+    res => res,
+    async err => {
+      const originalRequest = err.config;
+      if (err.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        const { data } = await api.post("/auth/refresh");
+        localStorage.setItem("token", data.accessToken);
+        api.defaults.headers.common["Authorization"] = `Bearer ${data.accessToken}`;
+        return api(originalRequest);
+      }
+      return Promise.reject(err);
+    }
+  );
+
 export default api;
